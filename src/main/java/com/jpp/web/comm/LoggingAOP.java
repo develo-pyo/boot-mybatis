71c3707b8f5d2261b234ffb630922e3cd3b6d2d2
develo-pyo <jungpyo9@gmail.com>
2020-02-10 15:30:26 +0900

    logging aop setting

--- src/main/java/com/jpp/web/comm/LoggingAOP.java
+++ src/main/java/com/jpp/web/comm/LoggingAOP.java
@@ -1,5 +1,50 @@
 package com.jpp.web.comm;
 
-public class LoggingAOP {
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 
+@Component
+@Aspect
+public class LoggingAOP {
+   
+   protected static final Logger logger = LoggerFactory.getLogger(LoggingAOP.class);
+   
+   @Around("execution(public * com.jpp.main.controller.*Controller.*(..))")
+   public ModelAndView mavAround(ProceedingJoinPoint pjp) {
+      ModelAndView mav = null;
+      
+      logger.info("aop works! 1");
+      System.out.println("????");
+      try {
+         pjp.proceed();
+      } catch (Throwable e) {
+         
+      }
+      logger.info("aop works! 2");
+      System.out.println("????");
+      
+      return mav;
+   }
+   
+   @Around("execution(public java.lang.String com.jpp.main.controller.*Controller.*(..))")
+   public String stringAround(ProceedingJoinPoint pjp)  {
+      
+      System.out.println("???? 1");
+      logger.info("aop works!");
+      try {
+         pjp.proceed();
+      } catch (Throwable e) {
+         
+      }
+      logger.info("aop works!");
+      System.out.println("???? 2");
+      
+      return "";
+   }
+   
 }

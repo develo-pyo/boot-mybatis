c1c12d59c4d78aa1b2020b45d8b349145978afc8
develo-pyo <jungpyo9@gmail.com>
2020-02-11 23:14:31 +0900

    convert application.xml to application.yml

--- src/main/java/com/jpp/Application.java
+++ src/main/java/com/jpp/Application.java
@@ -1,13 +1,25 @@
 package com.jpp;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
+//	public static void main(String[] args) {
+//		SpringApplication.run(Application.class, args);
+//	}
+	
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+         + "classpath:application.yml,"      // 덮어 쓸 파일(git 노출허용)
+         + "classpath:application_real.yml"; // 실제파일(.gitignore 용)
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+      //빌더패턴
+      new SpringApplicationBuilder(Application.class)
+          .properties(APPLICATION_LOCATIONS)
+          .run(args);
 	}
 	
 }
